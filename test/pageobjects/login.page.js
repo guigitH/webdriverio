a33a9dd0147f626ec9623c57b333ecec49662415
Guillaume <slack77@hotmail.fr>
2023-02-17 16:06:04 +0100

    Add dynamic control exemple

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,14 +1,8 @@
-
-
 const Page = require('./page');
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
+
 class LoginPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
+
     get inputUsername () {
         return $('#username');
     }
@@ -21,19 +15,20 @@ class LoginPage extends Page {
         return $('button[type="submit"]');
     }
 
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
+    get enableButton() {
+        return $('#input-example button')
+    }
+
+    get inputEnabledField() {
+        return $('#input-example input')
+    }
+
     async login (username, password) {
         await this.inputUsername.setValue(username);
         await this.inputPassword.setValue(password);
         await this.btnSubmit.click();
     }
 
-    /**
-     * overwrite specific options to adapt it to page object
-     */
     open () {
         return super.open('login');
     }
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -9,7 +9,18 @@ describe('My Login application', () => {
         await expect(SecurePage.flashAlert).toBeExisting()
         await expect(SecurePage.flashAlert).toHaveTextContaining(
             'You logged into a secure area!')
+        await expect(browser).toHaveUrl('https://the-internet.herokuapp.com/secure')
     })
 })
 
 
+describe('My dynamic control application', () => {
+    it('should wait for the input fild to be enabled', async () => {
+        await browser.url('https://the-internet.herokuapp.com/dynamic_controls')
+
+        await LoginPage.enableButton.waitForDisplayed()
+        await LoginPage.enableButton.click()
+        await LoginPage.inputEnabledField.waitForDisplayed({timeout: 4000})
+        await expect(LoginPage.inputEnabledField).toBeEnabled()
+    })
+})
